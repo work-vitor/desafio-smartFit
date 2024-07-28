5a8216f91a64afad027505edcdbf8632500778e1
Vitor Rodrigues <Vitor@email.com>
2024-07-28 01:02:34 -0300

    feat: form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   results = [];
+
+  formGroup!: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(): void{
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit(): void{
+    console.log(this.formGroup.value)
+  }
+
+  onClear(): void{
+    this.formGroup.reset()
+  }
 }
